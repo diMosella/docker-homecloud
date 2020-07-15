3cb6fb0a0ed666247397ba436d88f6ac2bf9d8c7
diMosella <wim.van.moosel@gmail.com>
2020-07-15 21:58:51 +0200

    ci(FLOW): working rollup config, next steps Dockerfile

--- flows/rollup.config.js
+++ flows/rollup.config.js
@@ -16,47 +16,69 @@ export default [
     external: ['os', 'cluster', 'child_process'],
     plugins: [
       replace({
-        // './src/services/serverWorkerProcess.mjs': './dist/serverWorker.mjs',
+        './src/services/soloWorkerProcess.mjs': './dist/soloWorker.mjs',
+        './src/services/serverWorkerProcess.mjs': './dist/serverWorker.mjs',
         './src/services/converterWorkerProcess.mjs': './dist/converterWorker.mjs',
-        // './src/services/soloWorkerProcess.mjs': './dist/soloWorker.mjs',
         delimiters: ['', '']
       }),
       json(), resolve({ preferBuiltins: true }), commonjs()]
   },
-  // {
-  //   input: 'src/services/soloWorkerProcess.mjs',
-  //   output: {
-  //     file: path.resolve(__dirname, 'dist', 'soloWorker.mjs'),
-  //     format: 'es'
-  //   },
-  //   external: [
-  //     'path', 'fs', 'cluster', 'child_process', 'util',
-  //     'crypto', 'http', 'https', 'stream', 'net', 'tls', 'zlib'
-  //   ],
-  //   plugins: [json(), resolve({ preferBuiltins: true }), commonjs()]
-  // },
-  // {
-  //   input: 'src/services/serverWorkerProcess.mjs',
-  //   output: {
-  //     file: path.resolve(__dirname, 'dist', 'serverWorker.mjs'),
-  //     format: 'es'
-  //   },
-  //   external: [
-  //     'path', 'fs', 'cluster', 'child_process', 'util',
-  //     'crypto', 'http', 'stream', 'net', 'tty', 'constants'
-  //   ],
-  //   plugins: [json(), resolve({ preferBuiltins: true }), commonjs()]
-  // },
-  // {
-  //   input: 'src/services/converterWorkerProcess.mjs',
-  //   output: {
-  //     file: path.resolve(__dirname, 'dist', 'converterWorker.mjs'),
-  //     format: 'es'
-  //   },
-  //   external: [
-  //     'path', 'fs', 'cluster', 'child_process',
-  //     'crypto', 'http', 'https', 'stream', 'net', 'tls', 'zlib'
-  //   ],
-  //   plugins: [json(), resolve({ preferBuiltins: false }), commonjs()]
-  // }
+  {
+    input: 'src/services/soloWorkerProcess.mjs',
+    output: {
+      file: path.resolve(__dirname, 'dist', 'soloWorker.mjs'),
+      format: 'es'
+    },
+    external: [
+      'path', 'fs', 'cluster', 'child_process', 'util', 'url', 'events',
+      'querystring', 'crypto', 'http', 'https', 'stream', 'net', 'tls',
+      'zlib', 'buffer', 'assert', 'string_decoder', 'punycode'
+    ],
+    plugins: [
+      replace({
+        'util.inherits(PrivateKey, Key);': 'setTimeout(() => { util.inherits(PrivateKey, Key); });',
+        delimiters: ['', '']
+      }),
+      json(), resolve({ preferBuiltins: true }), commonjs()
+    ]
+  },
+  {
+    input: 'src/services/serverWorkerProcess.mjs',
+    output: {
+      file: path.resolve(__dirname, 'dist', 'serverWorker.mjs'),
+      format: 'es'
+    },
+    external: [
+      'path', 'fs', 'cluster', 'child_process', 'util', 'url', 'events',
+      'crypto', 'http', 'stream', 'net', 'tty', 'constants', 'assert',
+      'querystring', 'buffer', 'os'
+    ],
+    plugins: [
+      replace({
+        'require(\'readable-stream/transform\')': 'require(\'stream\').Transform',
+        'require("readable-stream/transform")': 'require("stream").Transform',
+        'readable-stream': 'stream',
+        delimiters: ['', '']
+      }),
+      json(), resolve({ preferBuiltins: true }), commonjs()]
+  },
+  {
+    input: 'src/services/converterWorkerProcess.mjs',
+    output: {
+      file: path.resolve(__dirname, 'dist', 'converterWorker.mjs'),
+      format: 'es'
+    },
+    external: [
+      'path', 'fs', 'cluster', 'child_process', 'util', 'assert', 'net',
+      'crypto', 'url', 'stream', 'querystring', 'http', 'https', 'buffer',
+      'tls', 'string_decoder', 'zlib', 'events', 'punycode'
+    ],
+    plugins: [
+      replace({
+        'util.inherits(PrivateKey, Key);': 'setTimeout(() => { util.inherits(PrivateKey, Key); });',
+        delimiters: ['', '']
+      }),
+      json(), resolve({ preferBuiltins: true }), commonjs()
+    ]
+  }
 ];

e2996fa67b489cd6a8dad674aeb705a5d0055362
diMosella <wim.van.moosel@gmail.com>
2020-07-16 15:45:53 +0200

    ci(FLOW): improved healthCheck

--- flows/test/healthCheck.mjs
+++ flows/test/healthCheck.mjs
@@ -4,13 +4,28 @@ import http from 'http';
 
 const options = {
   host: 'localhost',
-  port : '8000',
-  timeout : 2000
+  port: '8000',
+  method: 'GET',
+  path: '/status',
+  timeout: 2000
 };
 
 const request = http.request(options, (response) => {
-  if (response.statusCode === 200 && response.body && response.body.success === true) {
-    process.exit(0);
+  if (response.statusCode === 200) {
+    let rawData = '';
+    response.on('data', (chunk) => { rawData += chunk; });
+    response.on('end', () => {
+      try {
+        const body = JSON.parse(rawData);
+        console.log(body);
+        if (body && body.success === true) {
+          process.exit(0);
+        }
+        process.exit(1);
+      } catch (_error) {
+        process.exit(1);
+      }
+    });
   } else {
     process.exit(1);
   }
